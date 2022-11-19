9fd3b036bd2edbe508d4488f45861b316e102bd0
VyPham23 <vy.pham200210@vnuk.edu.vn>
2022-11-19 14:49:26 +0700

    commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Navbar(props) {
+    return (
+        <div>
+            <nav className='nav'>
+                
+                <a href="#" className='brand'>
+                    <img src="" alt="" />
+                </a>
+            </nav>
+            <ul className="nav__menu"></ul>
+        </div>
+    );
+}
+
+export default Navbar;
