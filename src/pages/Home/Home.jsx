f3e04c924d53b45667caa18459d66f5f067bdb53
Harry <harrytruong1772@gmail.com>
2022-11-20 00:31:07 +0700

    feat: add homepage and navbar

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header } from '../../components'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='app__homepage'>
+        <Header />
+    </div>
+  )
+}
+
+export default Home
